7c7e0424b6e8d54828cbd5e9c1106d2512da9b6b
Garming Sam <garming@catalyst.net.nz>
2014-02-07 12:11:03 +1300

    Add license text to samba/gpclass.py
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>

--- python/samba/gpclass.py
+++ python/samba/gpclass.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
-# Copyright Luke Morrison <luc785@.hotmail.com> 2013
+#
+# Reads important GPO parameters and updates Samba
+# Copyright (C) Luke Morrison <luc785@.hotmail.com> 2013
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'''Reads important GPO parameters and updates Samba'''
+
 import sys
 import os
 sys.path.insert(0, "bin/python")
