0382c8d4363de5627d31335870cd6d3ac668b677
Andreas Schneider <asn@samba.org>
2015-04-22 15:39:45 +0200

    testprogs: Make bogus blackbox test working with MIT
    Signed-off-by: Andreas Schneider <asn@samba.org>

--- testprogs/blackbox/bogus.sh
+++ testprogs/blackbox/bogus.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]; then
 cat <<EOF
-Usage: blackbox_newuser.sh PREFIX
+Usage: bogus.sh SERVER SHARE USER PASSWORD DC_USER DC_PASSWORD SMBCLIENT
 EOF
 exit 1;
 fi
@@ -18,11 +18,18 @@ DC_PWD=$6
 smbclient=$7
 shift 7
 
+heimdal=0
+if test -x $BINDIR/samba4kinit; then
+	heimdal=1
+fi
+
 TEST_USER=bogus_testuser
 TEST_PWD=bogus_pass3#@
 net="$BINDIR/net"
 testit_expect_failure "smbclient" $smbclient "//$SERVER/$SHARE" -W POUET -U$DC_USER%$DC_PWD -c "dir"&& failed=`expr $failed + 1`
-testit "net.user.add" $net rpc user add $TEST_USER $TEST_PWD -W $SERVER -U$SERVER\\$USER%$PWD -S $SERVER
-testit "smbclient" $smbclient "//$SERVER/$SHARE" -W POUET -U$TEST_USER%$TEST_PWD -c "dir"|| failed=`expr $failed + 1`
-testit "net.user.delete" $net rpc user delete $TEST_USER -W $SERVER -U$SERVER\\$USER%$PWD -S $SERVER
+if test $heimdal -eq 1; then
+	testit "net.user.add" $net rpc user add $TEST_USER $TEST_PWD -W $SERVER -U$SERVER\\$USER%$PWD -S $SERVER
+	testit "smbclient2" $smbclient "//$SERVER/$SHARE" -W POUET -U$TEST_USER%$TEST_PWD -c "dir"|| failed=`expr $failed + 1`
+	testit "net.user.delete" $net rpc user delete $TEST_USER -W $SERVER -U$SERVER\\$USER%$PWD -S $SERVER
+fi
 exit $failed
