0c0551ce658f8e6566219676516b05fabc98a6da
David Mulder <dmulder@suse.com>
2017-08-09 09:25:53 -0600

    gpo: Add machine policies
    Add environment variable machine policies
    
    Signed-off-by: David Mulder <dmulder@suse.com>

---
+++ python/samba/gp_env_var_ext.py
@@ -0,0 +1,102 @@
+from gpclass import gp_ext, file_to
+from gp_file_append import ini_file_append
+import xml.etree.ElementTree as etree
+import re, os, os.path
+
+sysconfdir = '/etc'
+
+class xml_to_env(file_to):
+    @staticmethod
+    def get_local_env():
+        global sysconfdir
+        return ini_file_append(os.path.join(sysconfdir, 'profile'))
+
+    def set_env_var(self, val):
+        global sysconfdir
+        if type(val) is not dict:
+            val = { 'value': val, 'action': 'U' }
+        profile = ini_file_append(os.path.join(sysconfdir, 'profile'))
+
+        old_val = profile[self.attribute]
+        self.logger.info('Environment Variable %s was changed from %s to %s' \
+                         % (self.attribute, old_val, val['value']))
+
+        if val['value'] == None:
+            del profile[self.attribute]
+        # Update, Replace, and Create all really do the same thing
+        elif val['action'] == 'U' or \
+             val['action'] == 'R' or \
+             val['action'] == 'C':
+            if self.attribute == 'PATH' and \
+               'partial' in val and \
+               val['partial'] == '1':
+                profile[self.attribute] = '$PATH:%s' % val['value']
+            else:
+                profile[self.attribute] = val['value']
+        # Remove should blank the variable
+        elif val['action'] == 'D':
+            profile[self.attribute] = ''
+
+        self.gp_db.store(str(self), self.attribute, old_val)
+
+    def mapper(self):
+        return self
+
+    def __getitem__(self, key):
+        return (self.set_env_var, self.explicit)
+
+    def __str__(self):
+        return 'Environment'
+
+class env_var_unapply_map:
+    def __getitem__(self, key):
+        return (key, xml_to_env)
+
+    def __contains__(self, key):
+        return True
+
+class gp_environment_variable_ext(gp_ext):
+    def __init__(self, logger, sysconfdir_in):
+        global sysconfdir
+        super(gp_environment_variable_ext, self).__init__(logger)
+        sysconfdir = sysconfdir_in
+
+    def list(self, rootpath):
+        return os.path.join(rootpath,
+           'MACHINE/Preferences/EnvironmentVariables/EnvironmentVariables.xml')
+
+    def apply_map(self):
+        if not hasattr(self, 'xml_conf'):
+            return { 'Environment' : env_var_unapply_map() }
+        else:
+            return {
+                'Environment' : {
+                    a.attrib['name']: (a.attrib['name'], xml_to_env) \
+                        for a in self.xml_conf.findall('EnvironmentVariable')
+                }
+            }
+
+    def read(self, policy):
+        self.xml_conf = etree.fromstring(policy)
+        apply_map = self.apply_map()['Environment']
+        ret = False
+
+        for env_var in self.xml_conf.findall('EnvironmentVariable'):
+            (att, setter) = apply_map.get(env_var.attrib['name'])
+            ret = True
+            setter(self.logger,
+                   self.ldb,
+                   self.gp_db,
+                   self.lp,
+                   att,
+                   env_var.find('Properties').attrib).update_samba()
+            self.gp_db.commit()
+        return ret
+
+    def __str__(self):
+        return 'Environment GPO extension'
+
+    @staticmethod
+    def disabled_file():
+        return os.path.splitext(os.path.abspath(__file__))[0] + '.py.disabled'
+
