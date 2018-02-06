b68ca05cb83fdc5ba87f360e9a09f6b74cdbd9a6
Noel Power <noel.power@suse.com>
2018-02-06 15:57:37 +0000

    handle utf8 encoding in read of metadata to write generated code

--- source4/scripting/bin/gen_werror.py
+++ source4/scripting/bin/gen_werror.py
@@ -23,87 +23,89 @@
 
 import sys, os.path, io, string
 from gen_error_common import parseErrorDescriptions, ErrorDef
+from io import open
+from builtins import str
 
 def generateHeaderFile(out_file, errors):
-    out_file.write("/*\n")
-    out_file.write(" * Descriptions for errors generated from\n")
-    out_file.write(" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
-    out_file.write(" */\n\n")
-    out_file.write("#ifndef _WERR_GEN_H\n")
-    out_file.write("#define _WERR_GEN_H\n")
+    out_file.write(u"/*\n")
+    out_file.write(u" * Descriptions for errors generated from\n")
+    out_file.write(u" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
+    out_file.write(u" */\n\n")
+    out_file.write(u"#ifndef _WERR_GEN_H\n")
+    out_file.write(u"#define _WERR_GEN_H\n")
     for err in errors:
-        line = "#define %s W_ERROR(%s)\n" % (err.err_define, hex(err.err_code))
+        line = u"#define %s W_ERROR(%s)\n" % (str(err.err_define), str(hex(err.err_code)))
         out_file.write(line)
-    out_file.write("\n#endif /* _WERR_GEN_H */\n")
+    out_file.write(u"\n#endif /* _WERR_GEN_H */\n")
 
 def generateSourceFile(out_file, errors):
-    out_file.write("#include \"werror.h\"\n")
+    out_file.write(u"#include \"werror.h\"\n")
 
-    out_file.write("/*\n")
-    out_file.write(" * Names for errors generated from\n")
-    out_file.write(" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
-    out_file.write(" */\n")
+    out_file.write(u"/*\n")
+    out_file.write(u" * Names for errors generated from\n")
+    out_file.write(u" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
+    out_file.write(u" */\n")
 
-    out_file.write("static const struct werror_code_struct dos_errs[] = \n")
-    out_file.write("{\n")
+    out_file.write(u"static const struct werror_code_struct dos_errs[] = \n")
+    out_file.write(u"{\n")
     for err in errors:
-        out_file.write("\t{ \"%s\", %s },\n" % (err.err_define, err.err_define))
-    out_file.write("{ 0, W_ERROR(0) }\n")
-    out_file.write("};\n")
+        out_file.write(u"\t{ \"%s\", %s },\n" % (str(err.err_define), str(err.err_define)))
+    out_file.write(u"{ 0, W_ERROR(0) }\n")
+    out_file.write(u"};\n")
 
-    out_file.write("\n/*\n")
-    out_file.write(" * Descriptions for errors generated from\n")
-    out_file.write(" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
-    out_file.write(" */\n")
+    out_file.write(u"\n/*\n")
+    out_file.write(u" * Descriptions for errors generated from\n")
+    out_file.write(u" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
+    out_file.write(u" */\n")
 
-    out_file.write("static const struct werror_str_struct dos_err_strs[] = \n")
-    out_file.write("{\n")
+    out_file.write(u"static const struct werror_str_struct dos_err_strs[] = \n")
+    out_file.write(u"{\n")
     for err in errors:
         # Account for the possibility that some errors may not have descriptions
         if err.err_string == "":
             continue
-        out_file.write("\t{ %s, \"%s\" },\n"%(err.err_define, err.err_string))
-    out_file.write("\t{ W_ERROR(0), 0 }\n")
-    out_file.write("};")
+        out_file.write(u"\t{ %s, \"%s\" },\n"%(str(err.err_define), str(err.err_string)))
+    out_file.write(u"\t{ W_ERROR(0), 0 }\n")
+    out_file.write(u"};")
 
 def generatePythonFile(out_file, errors):
-    out_file.write("/*\n")
-    out_file.write(" * Errors generated from\n")
-    out_file.write(" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
-    out_file.write(" */\n")
-    out_file.write("#include <Python.h>\n")
-    out_file.write("#include \"python/py3compat.h\"\n")
-    out_file.write("#include \"includes.h\"\n\n")
-    out_file.write("static inline PyObject *ndr_PyLong_FromUnsignedLongLong(unsigned long long v)\n");
-    out_file.write("{\n");
-    out_file.write("\tif (v > LONG_MAX) {\n");
-    out_file.write("\t\treturn PyLong_FromUnsignedLongLong(v);\n");
-    out_file.write("\t} else {\n");
-    out_file.write("\t\treturn PyInt_FromLong(v);\n");
-    out_file.write("\t}\n");
-    out_file.write("}\n\n");
+    out_file.write(u"/*\n")
+    out_file.write(u" * Errors generated from\n")
+    out_file.write(u" * [MS-ERREF] https://msdn.microsoft.com/en-us/library/cc231199.aspx\n")
+    out_file.write(u" */\n")
+    out_file.write(u"#include <Python.h>\n")
+    out_file.write(u"#include \"python/py3compat.h\"\n")
+    out_file.write(u"#include \"includes.h\"\n\n")
+    out_file.write(u"static inline PyObject *ndr_PyLong_FromUnsignedLongLong(unsigned long long v)\n");
+    out_file.write(u"{\n");
+    out_file.write(u"\tif (v > LONG_MAX) {\n");
+    out_file.write(u"\t\treturn PyLong_FromUnsignedLongLong(v);\n");
+    out_file.write(u"\t} else {\n");
+    out_file.write(u"\t\treturn PyInt_FromLong(v);\n");
+    out_file.write(u"\t}\n");
+    out_file.write(u"}\n\n");
     # This is needed to avoid a missing prototype error from the C
     # compiler. There is never a prototype for this function, it is a
     # module loaded by python with dlopen() and found with dlsym().
-    out_file.write("static struct PyModuleDef moduledef = {\n")
-    out_file.write("\tPyModuleDef_HEAD_INIT,\n")
-    out_file.write("\t.m_name = \"werror\",\n")
-    out_file.write("\t.m_doc = \"WERROR defines\",\n")
-    out_file.write("\t.m_size = -1,\n")
-    out_file.write("};\n\n")
-    out_file.write("MODULE_INIT_FUNC(werror)\n")
-    out_file.write("{\n")
-    out_file.write("\tPyObject *m;\n\n")
-    out_file.write("\tm = PyModule_Create(&moduledef);\n");
-    out_file.write("\tif (m == NULL)\n");
-    out_file.write("\t\treturn NULL;\n\n");
+    out_file.write(u"static struct PyModuleDef moduledef = {\n")
+    out_file.write(u"\tPyModuleDef_HEAD_INIT,\n")
+    out_file.write(u"\t.m_name = \"werror\",\n")
+    out_file.write(u"\t.m_doc = \"WERROR defines\",\n")
+    out_file.write(u"\t.m_size = -1,\n")
+    out_file.write(u"};\n\n")
+    out_file.write(u"MODULE_INIT_FUNC(werror)\n")
+    out_file.write(u"{\n")
+    out_file.write(u"\tPyObject *m;\n\n")
+    out_file.write(u"\tm = PyModule_Create(&moduledef);\n");
+    out_file.write(u"\tif (m == NULL)\n");
+    out_file.write(u"\t\treturn NULL;\n\n");
     for err in errors:
-        line = """\tPyModule_AddObject(m, \"%s\",
-                  \t\tndr_PyLong_FromUnsignedLongLong(W_ERROR_V(%s)));\n""" % (err.err_define, err.err_define)
+        line = u"""\tPyModule_AddObject(m, \"%s\",
+                  \t\tndr_PyLong_FromUnsignedLongLong(W_ERROR_V(%s)));\n""" % (str(err.err_define), str(err.err_define))
         out_file.write(line)
-    out_file.write("\n");
-    out_file.write("\treturn m;\n");
-    out_file.write("}\n");
+    out_file.write(u"\n");
+    out_file.write(u"\treturn m;\n");
+    out_file.write(u"}\n");
 
 def transformErrorName( error_name ):
     if error_name.startswith("WERR_"):
@@ -135,20 +137,20 @@ def main():
         print("usage: %s winerrorfile headerfile sourcefile pythonfile" % sys.argv[0])
         sys.exit()
 
-    input_file = open(input_file_name, "r")
+    input_file = open(input_file_name, "r", encoding='utf8')
     errors = parseErrorDescriptions(input_file, True, transformErrorName)
     input_file.close()
 
     print("writing new header file: %s" % gen_headerfile_name)
-    out_file = open(gen_headerfile_name, "w")
+    out_file = open(gen_headerfile_name, "w", encoding='utf8')
     generateHeaderFile(out_file, errors)
     out_file.close()
     print("writing new source file: %s" % gen_sourcefile_name)
-    out_file = open(gen_sourcefile_name, "w")
+    out_file = open(gen_sourcefile_name, "w", encoding='utf8')
     generateSourceFile(out_file, errors)
     out_file.close()
     print("writing new python file: %s" % gen_pythonfile_name)
-    out_file = open(gen_pythonfile_name, "w")
+    out_file = open(gen_pythonfile_name, "w", encoding='utf8')
     generatePythonFile(out_file, errors)
     out_file.close()
 
