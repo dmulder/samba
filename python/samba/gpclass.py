367498aa88212ccbae2de845e5ccde312c1ea693
David Mulder <dmulder@suse.com>
2018-05-09 13:16:38 -0600

    gpo: Implement process_group_policy() gp_ext func
    MS spec describes the policy callback as a
    function called ProcessGroupPolicy which accepts
    a pDeletedGPOList and a pChangedGPOList param.
    The Group Policy Client Side Extension then
    iterates over the deleted, then the changed gpo
    lists and applies/unapplies policy. We should do
    this also.
    
    Signed-off-by: David Mulder <dmulder@suse.com>

--- python/samba/gpclass.py
+++ python/samba/gpclass.py
@@ -327,7 +327,7 @@ class gp_ext(object):
         self.gp_db = store.get_gplog(creds.get_username())
 
     @abstractmethod
-    def list(self, rootpath):
+    def process_group_policy(self, deleted_gpo_list, changed_gpo_list):
         pass
 
     @abstractmethod
@@ -376,7 +376,7 @@ class gp_ext_setter():
 
 class gp_inf_ext(gp_ext):
     @abstractmethod
-    def list(self, rootpath):
+    def process_group_policy(self, deleted_gpo_list, changed_gpo_list):
         pass
 
     @abstractmethod
@@ -484,30 +484,33 @@ def apply_gp(lp, creds, logger, store, gp_extensions):
             % dc_hostname)
         return
 
+    changed_gpos = []
     for gpo_obj in gpos:
-        guid = gpo_obj.name
-        if guid == 'Local Policy':
+        if not gpo_obj.file_sys_path:
             continue
-        path = os.path.join(lp.get('realm').lower(), 'policies', guid)
+        guid = gpo_obj.name
+        path = check_safe_path(gpo_obj.file_sys_path.lower())
         version = gpo_version(lp, path)
         if version != store.get_int(guid):
             logger.info('GPO %s has changed' % guid)
-            gp_db.state(GPOSTATE.APPLY)
-        else:
-            gp_db.state(GPOSTATE.ENFORCE)
-        gp_db.set_guid(guid)
-        store.start()
-        for ext in gp_extensions:
-            try:
-                ext.parse(ext.list(path))
-            except Exception as e:
-                logger.error('Failed to parse gpo %s for extension %s' % \
-                    (guid, str(ext)))
-                logger.error('Message was: ' + str(e))
-                store.cancel()
-                continue
+            changed_gpos.append(gpo_obj)
+
+    store.start()
+    for ext in gp_extensions:
+        try:
+            ext.process_group_policy([], changed_gpos)
+        except Exception as e:
+            logger.error('Failed to apply extension  %s' % str(ext))
+            logger.error('Message was: ' + str(e))
+            continue
+    for gpo_obj in gpos:
+        if not gpo_obj.file_sys_path:
+            continue
+        guid = gpo_obj.name
+        path = check_safe_path(gpo_obj.file_sys_path.lower())
+        version = gpo_version(lp, path)
         store.store(guid, '%i' % version)
-        store.commit()
+    store.commit()
 
 def unapply_log(gp_db):
     while True:
